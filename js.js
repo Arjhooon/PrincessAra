f3aba94a615b412c4503b9bcdfa61eadba401890
Arjhooon <140694475+Arjhooon@users.noreply.github.com>
2023-11-04 17:27:39 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function () {
+    var loadingScreen = document.querySelector('.loading-screen');
+    loadingScreen.style.display = 'none';
+    
+    document.addEventListener('click', function () {
+        window.location.href = 'second.html';
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
